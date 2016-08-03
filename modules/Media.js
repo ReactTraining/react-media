25ac751573cb1312a8a92f2d3af1c6c1335efd74
Michael Jackson <mjijackson@gmail.com>
2016-08-02 21:25:04 -0700

    Initial commit

---
+++ modules/Media.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+
+class Media extends React.Component {
+  static propTypes = {
+    query: PropTypes.string.isRequired
+  }
+
+  state = {
+    matches: false
+  }
+
+  updateMatches = () =>
+    this.setState({ matches: this.mediaQueryList.matches })
+
+  componentWillMount() {
+    this.mediaQueryList = window.matchMedia(this.props.query)
+    this.mediaQueryList.addListener(this.updateMatches)
+    this.updateMatches()
+  }
+
+  componentWillUnmount() {
+    this.mediaQueryList.removeListener(this.updateMatches)
+  }
+
+  render() {
+    return this.state.matches ? React.Children.only(this.props.children) : null
+  }
+}
+
+export default Media
