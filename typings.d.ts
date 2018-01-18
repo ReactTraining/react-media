463ebf114f1f6295cfc888e31ff9a489433d5f9b
Edo Rivai <edo.rivai@gmail.com>
2018-01-18 15:31:48 +0100

    Fix react TS typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,9 +1,14 @@
 declare module 'react-media' {
-  interface MediaProps {
-    query: string;
-    defaultMatches?: boolean;
-    children: ((matches: boolean) => JSX.Element) | JSX.Element
-  }
+	export interface MediaQueryObject {
+		[id: string]: boolean | number | string;
+	}
 
-  export default class Media extends React.Component<MediaProps, any> {}
+	export interface MediaProps {
+		query: string | MediaQueryObject | MediaQueryObject[];
+		defaultMatches?: boolean;
+		children?: ((matches: boolean) => React.ReactNode) | React.ReactNode;
+		render?: () => React.ReactNode;
+	}
+
+	export default class Media extends React.Component<MediaProps, any> {}
 }
