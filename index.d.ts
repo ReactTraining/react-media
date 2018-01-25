37aa81c350eac95bc2d8779400959abbdf87767b
Edo Rivai <edo.rivai@gmail.com>
2018-01-25 18:08:47 +0100

    Remove `declare module` statement

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,14 @@
 import { ReactNode, Component } from 'react';
 
-declare module 'react-media' {
-  export interface MediaQueryObject {
-    [id: string]: boolean | number | string;
-  }
-
-  export interface MediaProps {
-    query: string | MediaQueryObject | MediaQueryObject[];
-    defaultMatches?: boolean;
-    children?: ((matches: boolean) => ReactNode) | ReactNode;
-    render?: () => ReactNode;
-  }
-
-  export default class Media extends Component<MediaProps, any> {}
+export interface MediaQueryObject {
+  [id: string]: boolean | number | string;
 }
+
+export interface MediaProps {
+  query: string | MediaQueryObject | MediaQueryObject[];
+  defaultMatches?: boolean;
+  children?: ((matches: boolean) => ReactNode) | ReactNode;
+  render?: () => ReactNode;
+}
+
+export default class Media extends Component<MediaProps, any> {}
