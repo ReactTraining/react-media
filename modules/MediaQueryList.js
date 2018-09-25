5ded436ace50e6078ec80d2ecd1b8c69812505de
yiochen <chen.yiou1992@gmail.com>
2018-09-24 22:48:29 -0700

    #100, fixed unit test

--- modules/MediaQueryList.js
+++ modules/MediaQueryList.js
@@ -1,13 +1,19 @@
 export default class MediaQueryList {
   constructor(targetWindow, query, listener) {
-    const nativeMediaQueryList = targetWindow.matchMedia(query);
+    this.nativeMediaQueryList = targetWindow.matchMedia(query);
     this.active = true;
     // Safari doesn't clear up listener with removeListener
     // when the listener is already waiting in the event queue.
     // Having an active flag to make sure the listener is not called
     // after we removeListener.
-    this.cancellableListener = (...args) => this.active && listener(...args);
-    nativeMediaQueryList.addListener(this.cancellableListener);
+    this.cancellableListener = (...args) => {
+      this.matches = this.nativeMediaQueryList.matches;
+      if (this.active) {
+        listener(...args);
+      }
+    };
+    this.nativeMediaQueryList.addListener(this.cancellableListener);
+    this.matches = this.nativeMediaQueryList.matches;
   }
 
   cancel() {
