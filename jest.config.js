dd55849c8e5a9873accbea2d296e6ce9ac8f5284
Michael Jackson <michael@jackson.us>
2018-12-11 18:09:39 -0800

    Test all builds including CJS and UMD

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,22 @@
+let mappedModule;
+switch (process.env.TEST_ENV) {
+  case 'cjs':
+    mappedModule = '<rootDir>/cjs/react-media.js';
+    break;
+  case 'umd':
+    mappedModule = '<rootDir>/umd/react-media.js';
+    break;
+  default:
+    mappedModule = '<rootDir>/modules/index.js';
+}
+
 module.exports = {
   globals: {
     __DEV__: true
-  }
+  },
+  moduleNameMapper: {
+    '^react-media$': mappedModule
+  },
+  testMatch: ['**/__tests__/**/*-test.js'],
+  testURL: 'http://localhost/'
 };
